1506777c3450f6e1e8d8273a8bf249af9c85951f
anh-may2k6 <hoangquocanh2n6@gmail.com>
2025-07-27 15:47:42 +0700

    Update test_1.cpp
    hehe

--- test_1.cpp
+++ test_1.cpp
@@ -8,11 +8,11 @@ int main(){
     int l = 0;
     string s;
     cin >> s;
-    size_t pos_1 = s.find('A');
-    int start = (pos_1 != string::npos) ? static_cast<int> (pos_1) : -1;
-    size_t pos_2 = s.rfind('Z');
+    size_t pos_1 = s.find('A'); // tìm kiếm và chọn A ở bên trái 
+    int start = (pos_1 != string::npos) ? static_cast<int> (pos_1) : -1; // ép kiểu int
+    size_t pos_2 = s.rfind('Z'); // tìm kiếm và chọn Z ở bên phải
     int end = (pos_2 != string::npos) ? static_cast <int> (pos_2) : -1;
-    l = end - start +1;
+    l = end - start +1; // tính độ dài dài nhất có thể
     cout << l << endl;
     return 0;
 }
